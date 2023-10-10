a30b830036bf961cce3d6244831b59aa3f348ebf
Mustapha Ayinde Abdul Aziz Akanji <144163133+Ayinde225@users.noreply.github.com>
2023-10-10 22:04:45 +0000

    1

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -1,15 +1,18 @@
-import { Client, Account, Databases } from 'appwrite';
+import { Client, Account, Databases } from "appwrite";
 
-export const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT
-export const PROJECT_ID = import.meta.env.VITE_PROJECT_ID
-export const DATABASE_ID = import.meta.env.VITE_DATABASE_ID
-export const COLLECTION_ID_MESSAGES = import.meta.env.VITE_COLLECTION_ID_MESSAGES
+export const API_ENDPOINT = import.meta.env.VITE_API_ENDPOINT;
+export const PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
+export const DATABASE_ID = import.meta.env.VITE_DATABASE_ID;
+export const COLLECTION_ID_MESSAGES = import.meta.env
+  .VITE_COLLECTION_ID_MESSAGES;
 
-const client = new Client()
-    .setEndpoint(API_ENDPOINT) 
-    .setProject(PROJECT_ID);    
+const client = new Client();
 
+client
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject("6525c1db7d137edf8de4");
+  
 export const account = new Account(client);
-export const databases = new Databases(client)
+export const databases = new Databases(client);
 
 export default client;
